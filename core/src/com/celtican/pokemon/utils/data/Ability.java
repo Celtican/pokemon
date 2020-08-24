a7a00ae2e093eec1b1ed0712eeefe1079e58f992
Celtican <Celtican5@mail.com>
2020-08-24 17:42:48 -0400

    Added getters to Ability class
    + Ability has getter methods for index, name, and description
    
    - Ability's index, name, and description are now private

--- core/src/com/celtican/pokemon/utils/data/Ability.java
+++ core/src/com/celtican/pokemon/utils/data/Ability.java
@@ -1,12 +1,22 @@
 package com.celtican.pokemon.utils.data;
 
 public class Ability {
-    public final int index;
-    public final String name, description;
+    private final int index;
+    private final String name, description;
 
     public Ability(int index, String name, String description) {
         this.index = index;
         this.name = name;
         this.description = description;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
     }
 }
